2e723ecb753ca891f524604962fcba61da6f544e
mauker <mauker_@hotmail.com>
2012-04-21 18:53:38 -0300

    Inicio do projeto

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<script type="text/javascript" src="js/jquery-last.js"></script>
+<script type="text/javascript" src="js/jquery_easing.js"></script>
+<title>Aiha</title>
+</head>
+
+<body>
+INDEX
+</body>
+</html>
